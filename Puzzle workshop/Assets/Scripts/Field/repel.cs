c83984d6b1276bfd4b290e58d5f9243974b486fd
ShreddedWombat <86807312+ShreddedWombat@users.noreply.github.com>
2021-10-01 11:01:04 +1300

    Transferring bounce prog to universal script

---
+++ Puzzle workshop/Assets/Scripts/Field/repel.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class repel : MonoBehaviour
+{   
+    
+    public GameObject playerBody;
+    public LayerMask fieldMask;
+    public float fieldDistance = 1f;
+    public Transform fieldCheck;
+    public Transform field;
+    
+    private Player_Movement2 playMove;
+
+    bool fieldKeep;
+    bool isFielded;
+    
+    Vector3 startPt;
+    Vector3 endPt;
+    Vector3 fieldNormal;
+    Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playMove = playerBody.GetComponent<Player_Movement2>;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isFielded = playMove.isFielded;
+        velocity = playMove.velocity;
+
+        startPt = new Vector3(fieldCheck.position.x, fieldCheck.position.y + (fieldDistance +1.5f), fieldCheck.position.z);
+        endPt = new Vector3(fieldCheck.position.x, fieldCheck.position.y - (fieldDistance +1.5f), playerBody.position.z);
+
+        if(fieldKeep == true)
+        {
+            isFielded = Physics.CheckCapsule(startPt, endPt, fieldDistance + 0.3f, fieldMask);
+            /*if(isFielded){
+                groundField = true;
+            }*/
+        }
+        else{
+            isFielded = false;
+            fieldTime += Time.deltaTime;
+            
+        }
+
+        if(isFielded && fieldKeep)
+          {
+              float xBounce;
+              float yBounce;
+              float zBounce;
+
+              if(velocity.x < 0){
+                    xBounce = 1;
+              }
+              else{
+                    xBounce = -1;}
+
+              if(velocity.y < 0){
+                    yBounce = 1;
+              }
+              else{
+                    yBounce = -1;}
+
+              if(velocity.z < 0){
+                    zBounce = 1;
+              }
+              else{
+                    zBounce = -1;}
+              //float xBounce = ((velocity.x) / (Mathf.Abs(velocity.x))) * 2;
+              //float yBounce = ((velocity.x) / (Mathf.Abs(velocity.y))) * 2;
+              //float zBounce = ((velocity.x) / (Mathf.Abs(velocity.z))) * 2;
+              Debug.Log(xBounce + " " + yBounce + " " + zBounce);
+              /*velocity.y = (velocity.y + yBounce) * -1;
+              velocity.x = (velocity.x + xBounce) * -1;
+              velocity.z = (velocity.z + zBounce) * -1;*/
+
+              Vector3 fieldNormal = (fieldCheck.position - field.position);
+              velocity = Vector3.Reflect(velocity , fieldNormal) / 40;
+              if(velocity.y > 30)
+              {
+                    velocity.y = 30;
+              }
+
+              fieldKeep = false;
+              groundField = true;
+          }
+
+          if(fieldTime > 0.3 || groundField == false){
+                fieldKeep = true;
+                fieldTime = 0;
+            }
+
+        playMove.isFielded = isFielded;
+        playMove.velocity = velocity;
+    }
+}
--- Puzzle workshop/Assets/Scripts/Player/Player_Movement2.cs
+++ Puzzle workshop/Assets/Scripts/Player/Player_Movement2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_Movement : MonoBehaviour
+public class Player_Movement2 : MonoBehaviour
 {
 
     public CharacterController controller;
@@ -17,20 +17,15 @@ public class Player_Movement : MonoBehaviour
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
-    public LayerMask fieldMask;
-    public float fieldDistance = 1f;
-    public Transform fieldCheck;
-    public Transform field;
+    
+    
 
-    Vector3 startPt;
-    Vector3 endPt;
-    Vector3 fieldNormal;
+    
 
     Vector3 velocity;
     bool isGrounded;
-    bool isFielded = false;
-    bool fieldKeep = true;
-    bool groundField = false;
+    public bool isFielded = false;
+    public bool groundField = false;
 
     float fieldTime = 0;
     // Update is called once per frame
@@ -44,73 +39,15 @@ public class Player_Movement : MonoBehaviour
             velocity.y = -2.6f;
 
         }
-
-        startPt = new Vector3(fieldCheck.position.x, fieldCheck.position.y + (fieldDistance +1.5f), fieldCheck.position.z);
-        endPt = new Vector3(fieldCheck.position.x, fieldCheck.position.y - (fieldDistance +1.5f), playerBody.position.z);
-
-        if(fieldKeep == true)
-        {
-            isFielded = Physics.CheckCapsule(startPt, endPt, fieldDistance + 0.3f, fieldMask);
-            /*if(isFielded){
-                groundField = true;
-            }*/
-        }
-        else{
-            isFielded = false;
-            fieldTime += Time.deltaTime;
-            
-        }
+        
 
         
         //isFielded = Physics.CheckSphere(groundCheck.position, groundDistance, fieldMask);
-          if(isFielded && fieldKeep)
-          {
-              float xBounce;
-              float yBounce;
-              float zBounce;
-
-              if(velocity.x < 0){
-                    xBounce = 1;
-              }
-              else{
-                    xBounce = -1;}
-
-              if(velocity.y < 0){
-                    yBounce = 1;
-              }
-              else{
-                    yBounce = -1;}
-
-              if(velocity.z < 0){
-                    zBounce = 1;
-              }
-              else{
-                    zBounce = -1;}
-              //float xBounce = ((velocity.x) / (Mathf.Abs(velocity.x))) * 2;
-              //float yBounce = ((velocity.x) / (Mathf.Abs(velocity.y))) * 2;
-              //float zBounce = ((velocity.x) / (Mathf.Abs(velocity.z))) * 2;
-              Debug.Log(xBounce + " " + yBounce + " " + zBounce);
-              /*velocity.y = (velocity.y + yBounce) * -1;
-              velocity.x = (velocity.x + xBounce) * -1;
-              velocity.z = (velocity.z + zBounce) * -1;*/
-
-              Vector3 fieldNormal = (fieldCheck.position - field.position);
-              velocity = Vector3.Reflect(velocity , fieldNormal) / 40;
-              if(velocity.y > 30)
-              {
-                    velocity.y = 30;
-              }
-
-              fieldKeep = false;
-              groundField = true;
-          }
+          
 
         
 
-        if(fieldTime > 0.3 || groundField == false){
-                fieldKeep = true;
-                fieldTime = 0;
-            }
+        
 
         if(groundField && isGrounded)
         {
