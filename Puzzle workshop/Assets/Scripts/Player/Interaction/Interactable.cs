1b497e303996fd44227640be5c045df34125c650
ShreddedWombat <86807312+ShreddedWombat@users.noreply.github.com>
2021-11-12 11:20:41 +1300

    Fixed Drop-Out-World bug

--- Puzzle workshop/Assets/Scripts/Player/Interaction/Interactable.cs
+++ Puzzle workshop/Assets/Scripts/Player/Interaction/Interactable.cs
@@ -9,7 +9,7 @@ public class Interactable : MonoBehaviour
 
     void OnDrawGizmosSelected()
     {
-        Gizmos.color = Color.yellow;
+        Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(transform.position, radius);
     }
 
--- Puzzle workshop/Assets/Scripts/Player/Interaction/Pickup.cs
+++ Puzzle workshop/Assets/Scripts/Player/Interaction/Pickup.cs
@@ -6,33 +6,55 @@ public class Pickup : MonoBehaviour
 {
     public Transform playerPos;
     public Transform dest;
+    public Transform drop;
+    public LayerMask ground;
+    public float rad = 1.5f;
     bool tog = false;
     bool again = false;
+    bool groundColl = false;
     int distance = 7;
 
     public float throwForce = 600;
     Vector3 objectPos;
 
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, rad);
+    }
+
     void OnMouseDown()
     { 
-        if (tog == false)
+        
+        if (Vector3.Distance(playerPos.position, transform.position) <= distance && !tog)
         {
-            if (Vector3.Distance(playerPos.position, transform.position) <= distance)
-            {
-                GetComponent<Collider>().enabled = false;
-                GetComponent<Rigidbody>().useGravity = false;
-                GetComponent<Rigidbody>().detectCollisions = true;
-                tog = true;
-            }
+            GetComponent<Collider>().enabled = false;
+            GetComponent<Rigidbody>().useGravity = false;
+            GetComponent<Rigidbody>().detectCollisions = true;
+            tog = true;
         }
         
+        
+    }
+
+    
+
+    void isGrounded(){
+        if(Physics.CheckSphere(transform.position, rad, ground))
+        {
+            groundColl = true;
+        }
+        else
+        {
+            groundColl = false;
+        }
     }
 
     void Update()
     {
-        
+        isGrounded();
 
-        if (tog == true)
+        if (tog)
         {
             GetComponent<Rigidbody>().velocity = Vector3.zero;
             GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
@@ -41,27 +63,27 @@ public class Pickup : MonoBehaviour
 
 
 
-            if (Input.GetButtonDown("Fire1"))
+            if (Input.GetButtonDown("Fire1") && !groundColl)
             {
-                if (again == true)
+                if (again)
                 {
-                    
+                    transform.position = drop.position;
                     transform.parent = null;
                     GetComponent<Rigidbody>().useGravity = true;
                     GetComponent<Collider>().enabled = true;
                     tog = false;
                     again = false;
                 }
-                else if (again == false)
+                else if (!again)
                 {
                     again = true;
                 }
             }
-            else if (Input.GetButtonDown("Fire2"))
+            else if (Input.GetButtonDown("Fire2") && !groundColl)
             {
-                if (again == true)
+                if (again)
                 {
-                    
+                    transform.position = dest.position;
                     transform.parent = null;
                     GetComponent<Rigidbody>().useGravity = true;
                     GetComponent<Collider>().enabled = true;
@@ -69,7 +91,7 @@ public class Pickup : MonoBehaviour
                     again = false;
                     GetComponent<Rigidbody>().AddForce(dest.forward * throwForce);
                 }
-                else if (again == false)
+                else if (!again)
                 {
                     again = true;
                 }
