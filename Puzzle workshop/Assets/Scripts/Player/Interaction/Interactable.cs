a59dba0d94267ce526bbebafe43e54fb24c133bf
ShreddedWombat <86807312+ShreddedWombat@users.noreply.github.com>
2022-05-12 12:25:42 +1200

    new pickup based on spheres

--- Puzzle workshop/Assets/Scripts/Player/Interaction/Interactable.cs
+++ Puzzle workshop/Assets/Scripts/Player/Interaction/Interactable.cs
@@ -6,17 +6,17 @@ public class Interactable : MonoBehaviour
 {
     //sets radius and (unused as of yet) toggle value
     public float radius = 3f;
-    public bool toggle = false;
+    public bool hold = false;
+    int distance = 7;
 
     //function to draw the radius when selected in the editor, makes it easy to see player interaction distances
-    void OnDrawGizmosSelected()
+    void OnMouseDown()
     {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(transform.position, radius);
-    }
-
-    void Update()
-    {
-        
+    if (Vector3.Distance(playerPos.position, transform.position) <= distance)
+        {
+            GetComponent<Collider>().enabled = false;
+            GetComponent<Rigidbody>().useGravity = false;
+            GetComponent<Rigidbody>().detectCollisions = true;
+            tog = true;
     }
 }
