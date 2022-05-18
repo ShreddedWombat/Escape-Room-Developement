46523db8b9d02b023f66af87b0ddaade27e7b854
ShreddedWombat <86807312+ShreddedWombat@users.noreply.github.com>
2022-05-18 14:40:45 +1200

    Revert "fixed slight Pickup_Mk2 Error"
    This reverts commit 68a3bed4d4ed2a99f39b9ce85122853f5da8a7f2.

--- Puzzle workshop/Assets/Scripts/Player/Interaction/Pickup_Mk2.cs
+++ Puzzle workshop/Assets/Scripts/Player/Interaction/Pickup_Mk2.cs
@@ -51,7 +51,7 @@ public class Pickup_Mk2 : MonoBehaviour
     void Update()
     {
         isGrounded();
-        hold = holder.GetComponent<Interactable>().hold;
+        hold = holder.script.hold;
 
         if (Vector3.Distance(playerPos.position, transform.position) <= distance && !tog && hold)
         {
