a67485ef4df32f7f14499c1efdf6734b62682688
ShreddedWombat <86807312+ShreddedWombat@users.noreply.github.com>
2022-06-02 12:25:08 +1200

    gedrg

---
+++ Puzzle workshop/Assets/Scripts/Player/Interaction/Pickups/PickupCustom_Mk3.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupCustom_Mk3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void CheckPress()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
