0424e6de08556832589c668426e33168a50bac5f
ShreddedWombat <86807312+ShreddedWombat@users.noreply.github.com>
2022-08-03 10:44:15 +1200

    gerstner
    https://www.youtube.com/watch?v=Awd1hRpLSoI

--- My project/Assets/Phys Shaders/meshScripts/MeshGen.cs
+++ My project/Assets/Phys Shaders/meshScripts/MeshGen.cs
@@ -21,6 +21,11 @@ public class MeshGen : MonoBehaviour
         UpdateMesh();
     }
 
+    void Update()
+    {
+        
+    }
+
     // Update is called once per frame
     void CreateShape()
     {
@@ -45,15 +50,35 @@ public class MeshGen : MonoBehaviour
         {
             for(int x = 0; x <= xSize; x++)
             {
+                //float y = Mathf.PerlinNoise(x * .3f, z * .3f) * 2f;
                 vertices[i] = new Vector3(x, 0, z);
                 i++;
             }
         }
 
-        triangles = new int[3];
-        triangles[0] = 0;
-        triangles[1] = xSize + 1;
-        triangles[2] = 1;
+        triangles = new int[xSize * zSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+
+            }
+            vert++;
+        }
     }
 
     void UpdateMesh()
