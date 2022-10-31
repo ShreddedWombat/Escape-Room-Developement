f48b334b7937d724babe0b46128dac012bb606f2
ShreddedWombat <86807312+ShreddedWombat@users.noreply.github.com>
2022-11-01 10:59:49 +1300

    fishspawn works

--- Fish/Assets/Scripts/FishSpawn.cs
+++ Fish/Assets/Scripts/FishSpawn.cs
@@ -7,6 +7,18 @@ public class FishSpawn : MonoBehaviour
 
     int fishNum;
 
+    int fishType;
+
+    public GameObject fabFish;
+    public GameObject fabTrout;
+    public GameObject fabCod;
+    public GameObject fabSalmon;
+
+    public int pointFish;
+    public int pointTrout;
+    public int pointCod;
+    public int pointSalmon;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +33,26 @@ public class FishSpawn : MonoBehaviour
 
     private void OnEnable()
     {
-        fishNum = Random.Range(25, 70);
+        fishNum = Random.Range(25, 45);
+        for(int i = 0; i < fishNum; i++)
+        {
+            fishType = Random.Range(1,20);
+            if (fishType <= 9)
+            {
+                Instantiate(fabFish, transform);
+            }
+            else if (fishType <= 14)
+            {
+                Instantiate(fabTrout, transform);
+            }
+            else if (fishType <= 19)
+            {
+                Instantiate(fabCod, transform);
+            }
+            else
+            {
+                Instantiate(fabSalmon, transform);
+            }
+        }
     }
 }
--- Fish/Assets/Scripts/Startup.cs
+++ Fish/Assets/Scripts/Startup.cs
@@ -10,12 +10,15 @@ public class Startup : MonoBehaviour
 
     public GameObject menuCam;
     public GameObject hook;
+    public GameObject fishFolder;
 
     // Start is called before the first frame update
     void Start()
     {
         playCam.SetActive(false);
         player.GetComponent<Player_Movement3>().enabled = false;
+
+        fishFolder.SetActive(false);
 
         //hook.SetActive(false);
         menuCam.SetActive(true);
